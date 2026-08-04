9963b3501356b402b1433aac0b05e84469b47f6e
Lucas <lucas.vale.marx@gmail.com>
2026-08-04 19:43:10 -0300

    feat: função que adiciona tarefas

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import customtkinter as ctk
+from CTkMessagebox import CTkMessagebox
 
 # criando a janela
+
 janela = ctk.CTk()
 
 janela.title('Lista de Tarefas')
 janela.geometry('400x350')
 
-titulo = ctk.CTkLabel(janela, text='Tarefas:', font=('Times', 15))
+tarefas = []
+
+
+titulo = ctk.CTkLabel(janela, text='Tarefas:', font=('Times', 15, 'bold'))
 titulo.pack(pady=15)
+
+# CTkMessagebox(title = 'Teste da message box', message= 'testee')
+
+
+def abrir_dialogo():
+    texto = ctk.CTkInputDialog(text='Digite uma tarefa: ', title='Tarefas')
+    user = texto.get_input()
+    if user:
+        print('entrou!', user)
+
+botao = ctk.CTkButton(janela, text = 'abrir dialogo', command=abrir_dialogo)
+botao.pack(padx=15, pady=50)
 
 janela.mainloop()
