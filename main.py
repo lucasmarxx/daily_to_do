630c0bc621a9cce05434963fc8ebd9990d39e140
Lucas <lucas.vale.marx@gmail.com>
2026-08-04 23:54:59 -0300

    feat: adição de botão de remoção de tarefas, frame de tarefas

--- main.py
+++ main.py
@@ -4,19 +4,27 @@ from CTkMessagebox import CTkMessagebox
 # criando a janela
 
 janela = ctk.CTk()
+ctk.set_appearance_mode('dark')
 
 janela.title('Lista de Tarefas')
-janela.geometry('400x350')
+janela.geometry('650x450')
 
 tarefas = []
 
 def adicionar_tarefas():
     tarefa = tarefas_a_fazer.get()
 
-    if tarefa:
+    if tarefa not in tarefas:
         tarefas.append(tarefa)
-        texto_tarefa_add.configure(text='Tarefa adicionada.')
+        texto_tarefa_add.configure(text='Tarefa adicionada.', text_color='#005c69')
         print(tarefas)
+        tarefa_adicionada = ctk.CTkLabel(master=frame_tarefas, text=tarefa)
+        tarefa_adicionada.pack(pady=5, padx=5, side='left')
+    else:
+        texto_tarefa_add.configure(text='Tarefa existente.', text_color='red')
+
+def remover_tarefas():
+    ...
 
 titulo = ctk.CTkLabel(janela, text='Tarefas:', font=('Times', 15, 'bold'))
 titulo.pack(pady=15)
@@ -24,17 +32,14 @@ titulo.pack(pady=15)
 tarefas_a_fazer = ctk.CTkEntry(janela, placeholder_text='Tarefas a Fazer', font=('Times', 12))
 tarefas_a_fazer.pack(pady=10)
 
-# CTkMessagebox(title = 'Teste da message box', message= 'testee')
+frame_tarefas = ctk.CTkScrollableFrame(janela)
+frame_tarefas.pack(pady=20, padx=20, fill = 'x', expand=True)
 
+botao_add_tarefas = ctk.CTkButton(janela, text = 'Adicionar Tarefa', command=adicionar_tarefas)
+botao_add_tarefas.pack(padx=15, pady=15, side='left')
 
-# def abrir_dialogo():
-#     texto = ctk.CTkInputDialog(text='Digite uma tarefa: ', title='Tarefas')
-#     user = texto.get_input()
-#     if user:
-#         print('entrou!', user)
-
-botao = ctk.CTkButton(janela, text = 'Adicionar Tarefa', command=adicionar_tarefas)
-botao.pack(padx=15, pady=15)
+botao_remocao_tarefas = ctk.CTkButton(janela, text='Remover Tarefa', command=remover_tarefas)
+botao_remocao_tarefas.pack(padx=25, pady=25, side='right')
 
 texto_tarefa_add = ctk.CTkLabel(janela, text='', font=('Times', 12))
 texto_tarefa_add.pack(pady=5)
